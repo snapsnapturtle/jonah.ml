9a0dd4ff2dbd535abc84e448da35e0beb041f1b8
Jonah Möller <jonah.moeller@outlook.com>
2016-01-17 23:18:14 +0100

    implemented basic search

---
+++ assets/search.js
@@ -0,0 +1,68 @@
+/**
+ * Created by jonah on 17/01/16.
+ */
+(function() {
+    window.searchIndex = lunr(function () {
+        this.field('id');
+        this.field('title', {boost: 10});
+        this.field('description');
+    });
+
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", function () {
+        var data = JSON.parse(xhr.responseText);
+        window.searchData = data;
+
+        for (var i = 0; i < data.length; i++) {
+            window.searchIndex.add({id: i, title: data[i].title, description: data[i].description});
+        }
+
+        var queries = getQueryString(document.location.search);
+        if (typeof queries['query'] != 'undefined') {
+            performSearch(queries['query']);
+            document.getElementById('search-query').value = queries['query']
+            window.history.pushState("ac", "asc", "/search/");
+        }
+    }, false);
+    xhr.open('GET', '/search/data.json', true);
+    xhr.send();
+})();
+
+function performSearch(query) {
+    var results = window.searchIndex.search(query);
+    displayResults(results);
+}
+
+document.getElementById("site-search").addEventListener('submit', function () {
+    event.preventDefault();
+    performSearch(document.getElementById('search-query').value)
+});
+
+document.getElementById('search-query').addEventListener('keyup', function () {
+    event.preventDefault();
+    performSearch(document.getElementById('search-query').value);
+});
+
+
+function getQueryString(query) {
+    query = query.split('+').join(' ');
+    var params = {}, tokens, re = /[?&]?([^=]+)=([^&]*)/g;
+    while (tokens = re.exec(query)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+    return params;
+}
+
+function displayResults(resultList) {
+    var searchResults = document.getElementById('search-results');
+
+    if (resultList.length) {
+        searchResults.innerHTML = '';
+        resultList.forEach(function (result) {
+            var item = window.searchData[result.ref];
+            searchResults.innerHTML += '<li><a href="' + item.url + '">' + item.title + '</a></li>';
+        });
+    } else {
+        searchResults.innerHTML = '<li>No results found</li>';
+    }
+}
